1cb7818229b40f999db53eb5b57f81c19d91a8a1
Saket Nema <52635391+Saket-Nema@users.noreply.github.com>
2020-08-26 02:22:02 +0530

    Create card.test.js

---
+++ components/card.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import CardComponent from "./card";
+
+describe ("CardComponent", () => {
+it('It should render without errors', () => {
+    expect(true).toBe(true);
+})
+})
