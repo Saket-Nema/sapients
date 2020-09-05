7d4e160ee72fb22733812477186225154d1c0335
Saket Nema <52635391+Saket-Nema@users.noreply.github.com>
2020-09-06 00:08:00 +0530

    Delete card.test.js

--- components/card.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from 'enzyme';
-import CardComponent from "./card";
-
-describe ("CardComponent", () => {
-it('It should render without errors', () => {
-    //expect(true).toBe(true);
-    })
- })
